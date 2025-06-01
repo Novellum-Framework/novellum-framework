53c2081f5f21b6df0092b3cc50a0a3ffc00f06c8
AANDDARK <andriyworkkovalenko@gmail.com>
2025-06-01 15:24:28 +0300

    first version yuhhu

--- classes/novela.js
+++ classes/novela.js
@@ -1,41 +1,51 @@
 import { rl } from "../tools/readline.js";
-export class Novela{
-    currentyRoute;
-    storyRoutes;
-    endes;
-    constructor(initRoute){
-        this.currentyRoute = initRoute
-        this.storyRoutes = []
-        this.endes = []
-    }
-     changeRoute(newRoute) {
+export class Novela {
+  _currentRoute;
+  storyRoutes;
+  endes;
 
-        const found = this.currentyRoute.choice.find((e) => e.name === newRoute.name);
-        if (found) {
-            this.currentyRoute = newRoute;
-            console.log("Route changed to", newRoute.name);
-        } else {
-            throw Error("Route not found: " + newRoute.name);
-        } 
-    }
+  constructor(initRoute) {
+      this._currentRoute = initRoute;
+      this.storyRoutes = [];
+      this.endes = [];
+  }
 
-    async request() {
-        const reqText = this.currentyRoute.choice
+  get currentRoute() {
+      return this._currentRoute;
+  }
+
+  changeRoute(newRoute) {
+      const found = this._currentRoute.choice.find((e) => e.name === newRoute.name);
+      if (found) {
+          this._currentRoute = newRoute;
+          console.log("Route changed to", newRoute.name);
+      } else {
+          throw new Error("Route not found: " + newRoute.name);
+      }
+  }
+
+  async showGame() {
+      // Show story first
+      for (const line of this._currentRoute.story) {
+          console.log(`${line.sender}: ${line.text}`);
+      }
+
+      // Show choices
+      const reqText = this._currentRoute.choice
           .map((choice, index) => `${index + 1}) ${choice.text}`)
           .join('\n');
-        console.log(reqText);
-        const input = await this.ask();
-        return input;
-      }
-      ask(question) {
-        return new Promise((resolve) => {
+      console.log(reqText);
+
+      // Get input
+      const input = await this.ask("Choose an option: ");
+      return input;
+  }
+
+  ask(question) {
+      return new Promise((resolve) => {
           rl.question(question, (answer) => {
-            rl.close();
-            resolve(answer);
+              resolve(answer);
           });
-        });
-      }
-    get currentyRoute(){
-        return this.currentyRoute
-    }
-}   
+      });
+  }
+}
